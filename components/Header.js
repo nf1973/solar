a1bd68592b36bcea1c2b279a2db22fe2af526f64
nf1973 <56644716+nf1973@users.noreply.github.com>
2024-02-11 18:26:30 +0100

    improve small screen responsiveness

--- components/Header.js
+++ components/Header.js
@@ -5,23 +5,25 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between w-full">
       <div>
-        <h2 className="text-3xl font-bold text-teal-600">Solar Analysis</h2>
-        <p className="text-sm italic text-teal-600">
+        <h2 className="text-xl lg:text-3xl font-bold text-teal-600">
+          Solar Analysis
+        </h2>
+        <p className="text-xs lg:text-sm italic text-teal-600">
           Harnessing Sunlight, Illuminating Insights
         </p>
       </div>
-      <div className="flex items-center justify-end w-2/3 gap-6 font-semibold text-teal-600 ">
+      <div className="flex items-center justify-end w-2/3 gap-8 font-semibold text-sm lg:text-base text-teal-600 ">
         <ul className="hover:text-teal-400">
           <Link
             href="https://pvoutput.org/list.jsp?id=52326&sid=50829"
             target="_blank"
           >
-            PVOutput
+            <span className="hidden md:inline">PVOutput</span>
           </Link>
         </ul>
         <ul className="hover:text-teal-400">
           <Link href="https://github.com/nf1973/solar" target="_blank">
-            GitHub
+            <span className="hidden md:inline">Github</span>
           </Link>
         </ul>
         <ul className="hover:text-teal-400">
